6f3a371771b856b3e3b104608e492df20726f89e
mariuszgajewski13 <mariusz.gajewski@microsoft.wsei.edu.pl>
2021-12-06 17:22:22 +0100

    Muzzle flash effect

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -9,6 +9,8 @@ public class MouseLook : MonoBehaviour
 
     public Camera camera;
 
+    public float cameraClamp = 20f;
+
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -20,7 +22,7 @@ public class MouseLook : MonoBehaviour
         rotation.y += Input.GetAxis("Mouse X");
         rotation.x += -Input.GetAxis("Mouse Y");
         
-        rotation.x = Mathf.Clamp(rotation.x, -15f, 15f);
+        rotation.x = Mathf.Clamp(rotation.x, -cameraClamp, cameraClamp);
         
         transform.eulerAngles = new Vector2(0, rotation.y) * sensitivity;
         camera.transform.localRotation = Quaternion.Euler(rotation.x * sensitivity, 0, 0);
--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -4,14 +4,20 @@ public class Shooting : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    public float impactForce = 30f;
+    public float fireRate = 15f;
 
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+
+    private float nextTimeToFire = 0f;
 
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetKey(KeyCode.Mouse0) && Time.time >= nextTimeToFire)
         {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
         }
     }
@@ -31,6 +37,14 @@ public class Shooting : MonoBehaviour
             {
                 target.TakeDamege(damage);
             }
+
+            if(hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            GameObject impact =  Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impact, 2f);
         }
     }
 }
